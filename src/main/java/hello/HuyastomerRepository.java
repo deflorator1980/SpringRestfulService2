6cbd5d38d74a0c316c047204f404d364ee143fb3
Your Name <you@example.com>
2017-06-28 21:27:17 +0300

    try sqlite

--- src/main/java/hello/HuyastomerRepository.java
+++
@@ -1,8 +0,0 @@
-package hello;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface HuyastomerRepository extends CrudRepository<Huyastomer, Long> {
-
-//    List<Huyastomer> findByLastName(String lastName);
-}
